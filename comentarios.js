ea75d3161ecfad48b8322d30dda0e30ccc6eebc8
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-05-07 20:36:50 -0300

    Sentenças e comentários

---
+++ comentarios.js
@@ -0,0 +1,11 @@
+console.log('Aprendendo a fazer comentários')
+//Comentários de linha única
+console.log('Passo 01')
+//console.log('Passo 02')
+console.log('Passo 03')
+
+/*
+ Comentário de
+ multiplas
+ linhas
+*/
