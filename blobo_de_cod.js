428027c9df0eab3dc0f7ee1ee40a65f578a7af46
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-05-14 19:21:19 -0300

    Create blobo_de_cod.js

---
+++ blobo_de_cod.js
@@ -0,0 +1,14 @@
+//Bloco de código número 1
+{
+    console.log('Passo 01')
+    console.log('Passo 02')
+    console.log('Passo 03')
+}
+
+//Bloco de código número 2
+{
+    console.log('Passo 04')
+    console.log('Passo 05')
+}
+
+console.log('Fim')
