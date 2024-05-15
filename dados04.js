0a51989fcf6edb73c65f5687c791a70d36c7a2f8
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-05-15 19:39:12 -0300

    Aula 4 e desafio do Raio

---
+++ dados04.js
@@ -0,0 +1,8 @@
+//CONST é usado para uma variavel constante (que não pode sofre alteração)
+//Caso haja uma tentativa de alterar o valor de uma variavel CONSTANTE, o código da erro.
+const a = 3;
+// a = a + 20;
+b = 5;
+b = b + 3;
+console.log(a);
+console.log(b);
---
+++ desafio_area.js
@@ -0,0 +1,15 @@
+//PI = 3.141592
+//PI * raio * raio
+
+const PI = 3.141592;
+let raio = 12;
+let areaDaCircu = PI * raio * raio;
+//let areCircPI = Math.PI * Math.pow(raio);
+//let quad = Math.pow(2);
+
+console.log("O valor do Raio é: "+raio+".");
+console.log("O valor do PI descrito dentro da variavel é: "+PI+".");
+console.log("O valor do PI descrito no MATH é: "+Math.PI+".");
+console.log("O valor da circunferência é: "+areaDaCircu+"m2.");
+//console.log("O valor da circunferência usando MATH para PI e POW é: "+areCircPI+"m2.");
+//console.log(quad);
