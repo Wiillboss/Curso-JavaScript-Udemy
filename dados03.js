46bb66926e2047dbe463e7f827bb2ab6d11a91b8
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-05-15 18:48:53 -0300

    aula 3 sobre dados

---
+++ dados03.js
@@ -0,0 +1,12 @@
+let idade = 31;
+//para identificar o tipo do dado, usamos "TYPEOF"
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 3200.49;
+console.log(typeof salario);
+
+let estaChovendo = true // ou FALSE
+console.log(typeof estaChovendo);
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
