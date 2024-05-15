b71bdea028e01f4854a78f8d8b1c1b19b48321ed
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-05-15 17:53:20 -0300

    alteração do nome do arquivo

---
+++ dados02.js
@@ -0,0 +1,20 @@
+var nome = "Caneta Bic";
+var quantidade = 10;
+var preco = 6.2;
+var imposto = 1.5;
+var precoFInal = preco+imposto;
+let nomeCompleto = "Willian dos Santos Silva"
+
+console.log("Meu nome completo é: "+nomeCompleto);
+console.log(nome);
+console.log('Caneta');
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(preco+imposto);
+console.log(precoFInal);
+
+console.log(11);
+console.log(6.4);
+console.log(2.5);
+console.log(7.9);
