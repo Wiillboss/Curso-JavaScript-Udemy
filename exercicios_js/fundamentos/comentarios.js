160a3e65352016ebd3203778a2466e0d46cf677b
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-06-19 21:37:48 -0300

    arquivo de aula sobre comentários

---
+++ exercicios_js/fundamentos/comentarios.js
@@ -0,0 +1,17 @@
+// comentário de uma linha
+console.log('Linha 1');
+
+/*
+Comentário de várias linhas
+*/
+console.log('Linha 2');
+
+
+/*
+* Comentário de várias linhas.
+* Ambos fazendo a mesma coisa,
+* Este é apenas uma forma de destacar melhor o comentário o estilizando.
+*
+*/
+console.log('Linha 3');
+
