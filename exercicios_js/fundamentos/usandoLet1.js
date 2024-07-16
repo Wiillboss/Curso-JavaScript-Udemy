660cfb2a544a7b8fb96cf31ef0c99099edc3ea77
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-07-15 22:36:46 -0300

    Aula básica sobre LET

---
+++ exercicios_js/fundamentos/usandoLet1.js
@@ -0,0 +1,18 @@
+var numero = 1;
+{
+    let numero =2;
+    console.log(`Dentro =${numero}`)
+}
+console.log(`Fora =${numero}`)
+
+/*
+    Variaveis declaradas com VAR tem seus escopos:
+        -Global;
+        -Função;
+    
+    Variaveis declaradas com LET tem seus escopos:    
+        -Global;
+        -Função;
+        -Bloco;
+
+*/
