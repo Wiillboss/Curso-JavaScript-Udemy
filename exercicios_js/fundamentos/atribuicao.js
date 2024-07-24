4d69f081fcab3751f01402434c9dea3def2d76f1
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-07-24 19:35:11 -0300

    aula sobre atribuição

---
+++ exercicios_js/fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a // é a mesma coisa que (b = b + a)
+console.log(b)
+
+b -= 4 // é a mesma coisa que (b = b - a)
+console.log(b)
+
+b *= 2 // é a mesma coisa que (b = b * a)
+console.log(b)
+
+b /= 2 // é a mesma coisa que (b = b / a)
+console.log(b)
+
+b %= 2 // é a mesma coisa que (b = b % a) ... o modulo (%)vé o resto da divisão
+console.log(b)
