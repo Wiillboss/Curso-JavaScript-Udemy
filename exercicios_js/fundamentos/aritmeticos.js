aa290fe34898869ee5c9152f315470a2c39d7be8
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-08-27 19:55:21 -0300

    aula sobre operadores aritmeticos

---
+++ exercicios_js/fundamentos/aritmeticos.js
@@ -0,0 +1,14 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d /a
+const modulo = a % 2
+
+console.log(`A soma é igual: ${soma}.
+A subtração é igual: ${subtracao}.
+A multiplicação é igual: ${multiplicacao}.
+A divisão é igual: ${divisao}.
+O modudlo é igual: ${modulo}`
+)
