0498da312b525a7843c94fc249d4949929bae8cf
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-10-23 07:59:46 -0300

    aula sobre funções anonimas

---
+++ funcao/funcoesAnonimas.js
@@ -0,0 +1,35 @@
+//função anonima é uma função sem nome
+
+const soma = function (x ,y){
+    return x + y
+}
+
+const imprimirResultado = function (a, b, operacao = soma) {
+    console.log(operacao (a, b))
+}
+
+
+imprimirResultado(3, 4)
+imprimirResultado(3, 4, soma)
+imprimirResultado(3, 4, function(x, y) {
+    return x - y
+})
+imprimirResultado(3, 4, (x, y) => x * y) // função arrow (=>)
+
+
+const pessoa = {
+    falar: function (){
+        console.log('Olá')
+    }
+}
+
+pessoa.falar()
+
+//na nova versão do JS é possível fazer na forma abaixo retirando FUNCTION
+const pessoa2 = {
+    ablar(){
+        console.log('IAE!')
+    }
+}
+
+pessoa2.ablar()
