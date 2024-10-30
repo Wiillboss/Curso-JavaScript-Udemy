28e428085d12a7fbd3607648d4acd4ef34278780
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-10-29 21:33:18 -0300

    conclusão da aula de revisão de O.O.

--- objeto/objetoRevisao.js
+++ objeto/objetoRevisao.js
@@ -46,3 +46,23 @@ carro['proprietario'] ['endereço'] ['logradouro'] = 'Av Gigante' // também alt
 console.log('Abaixo segue somente os valores dos atributos alterados do número e logradouro (respectivamente) do proprieário do carro! ')
 console.log(carro.proprietario.endereço.numero, carro.proprietario.endereço.logradouro)
 // acima estou mostrando apenas os atributos que sofreram alteração
+
+console.log(carro.consutores.length)
+/*
+A linha acima foi inserida após as linhas abaixo apenas para ilustrar acima, como ainda não foi excluida, ela funciona
+Já a mesma linha inserida após os deletes, caso seja exibida, ocorre erro do código 
+*/
+
+
+// deletando alguns atributos do carro
+delete carro.consutores
+delete carro.proprietario.endereço
+delete carro.calcularValorDoSeguro
+console.log('Após deleta alguns atributos do carro, segue abaixo a exibição dos atributos restantes')
+console.log(carro)
+// abaixo segue uma exibição que foi deleta, neste caso, seu resultado será UNDEFINED
+console.log(carro.consutores)
+
+//console.log(carro.consutores.length)
+//Linha acimda da erro de código, viso que aqui os condutores já foram excluídos
+//Tentar acessar o comprimento de algo indefido, ocorre erro.
