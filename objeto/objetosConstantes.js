16c4413d5ba84742cd21aca9fb49fc98689ac33c
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-10-30 19:09:19 -0300

    aula sobre objeto constante

---
+++ objeto/objetosConstantes.js
@@ -0,0 +1,20 @@
+// pessoa -> 123 -> {...}
+const pessoa = {nome: 'João'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// pessoa -> 456 -> {...}
+// pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstate = Object.freeze({ nome:'João'})
+pessoaConstate.nome = 'Maria'
+console.log(pessoaConstate)
