8c47a6414fefe859f13dfc2545c64d004701c158
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-11-06 20:06:30 -0300

    comentários sobre aula 2 de herença

--- objeto/heranca2.js
+++ objeto/heranca2.js
@@ -3,11 +3,34 @@
 Object.prototype.attr0 = '0' // não faça isso em casa!
 
 const avo = { attr1: 'A'}
-const pai = { __proto__: avo, attr2: 'B', attr3: '3'}
-const filho = { __proto__: pai, attr3: 'C'}
+const pai = { __proto__: avo, attr2: 'B', attr3: '3'} // (__prot__ : avo) indica que o avo é prototipo (herença) do pai
+const filho = { __proto__: pai, attr3: 'C'} // (__proto__: pai) indica que o pai é prototipo (herença) do filho
+/*
+A partir do filho, caso pegue um atributo que o filho não tenha, ele vai procurar no pai, casso esse não tenha, vai procurar no avo.
+*/  
+
+console.log(filho.attr1) 
+/* Referente ao console acima:
+Filho tem atributo 1? Não, então vai procurar no pai, que tambem não tem, vai procurar no avo, esse sim tem.
+*/ 
+
+console.log(filho.attr0)
+/* Referente ao console acima:
+Filho tem atributo 1? Não, então vai procurar no pai, que tambem não tem, vai procurar no avo,
+ tambem não, ele vai procurar no Object Prototype, esse sim tem.
+*/
+
+console.log(filho.attr4)
+/* Referente ao console acima:
+Filho tem atributo 1? Não, então vai procurar no pai, que tambem não tem, vai procurar no avo,
+ tambem não, ele vai procurar no Object Prototype, que tambem não tem. Seu resultado vai ser UNDEFINED
+*/
 
 console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
-
+/* Referente ao console acima.
+Observe que existe dois atributos 3, um no filho e outro no pai.
+Neste caso o atributo que fica é o que está no filho
+*/
 
 const carro = {
     velAtual: 0,
