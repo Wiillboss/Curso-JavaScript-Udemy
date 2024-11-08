2b55d4593c7d5f5480a1d2eef154078ec43393a6
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-11-08 18:08:01 -0300

    Update jsonVsObj.js

--- objeto/jsonVsObj.js
+++ objeto/jsonVsObj.js
@@ -5,4 +5,5 @@ console.log(JSON.stringify(obj))
 
 //console.log(JSON.parse(" { a:1, b:2, c:3 } "))
 //console.log(JSON.parse(" {'a':1, 'b':2, 'c':3}"))
+console.log(JSON.parse(' {"a":1, "b":2, "c":3}'))
 console.log(JSON.parse( '{"a": 1.7, "b": "String", "c": true, "d": {}, "e": [] }'))
