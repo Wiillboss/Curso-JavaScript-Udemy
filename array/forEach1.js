850a5bb6cccce2297e7ba42626aeb9103275f3a3
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-11-12 16:10:28 -0300

    Add files via upload

---
+++ array/forEach1.js
@@ -0,0 +1,24 @@
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+// dentro da FUNCTION, caso queria ter acesso ou INDICE, é obrigado é incluir 2 paramentos para que o segundo seja obrigatoriamente o INDICE
+aprovados.forEach(function(nome, indice){
+    console.log(`${indice + 1} ${nome}`)
+})
+
+//incluindo terceiro parametro
+aprovados.forEach(function(nome, indice, array){
+    console.log(`${indice + 1} ${nome}`)
+    console.log(array)
+})
+
+//incluindo um quarto parametro para identificar que a funcção CALLBECK no forEach somente pode receber 3 parametros: nome; indice; array
+aprovados.forEach(function(nome, indice, array, x){
+    console.log(`${indice + 1} ${nome}`)
+    console.log(x)
+})
+
+// Abaixo usando ARROW FUNCTION 
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovado.forEach(exibirAprovados)
