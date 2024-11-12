0403ef960aef4b4b70efa297fd3e53969b605f8b
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2024-11-11 22:46:44 -0300

    conclusão a primeira aula sobre array

--- array/array.js
+++ array/array.js
@@ -28,11 +28,33 @@ console.log(aprovados.length)
 */
 console.log(aprovados [8] === undefined) // pergunta se o valor da posição é UNDEFINED. Sim é.
 console.log(aprovados [8] === null) // pergunta se o valor da posição é NULL. Não é.
+console.log('Antes do SORT')
 console.log(aprovados)
+
 aprovados.sort()
 /*
     Com relação ao SORT acima:
     Ajustando a ordenação do array em ordem alfabetica e deixando os elementos UNDEFINED após a ordenação,
      assim retornando um novo array.
+*/
+console.log('Após o SORT')
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(`Onde antes no indice 1 era ANA, agora é ${aprovados[1]} após o delete.
+     Os demais indices segue normalmente, veja abaixo o indice 2.`)
+console.log(aprovados[2])
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+aprovados.splice(1,1, 'Elemento1', 'Elemento2')
+aprovados.splice(3, 0, 'Elemento3') 
+/*
+    O splice serve tanto para adicionar, remover elementos e ambos ao mesmo tempo.
+    * Caso queira excluir elementos:
+    O primeiro número indica o indice, o segundo indica quantos elementos a partir do indice devem ser deletados.
+    Os demais em seguida são os elementos adicionados no array
+    * Caso queira somente adicionar elementos: 
+    O primeiro número indica o indice, o segundo (quando zero) indica quantos elementos a partir do indice devem ser inseridos.
+    Os demais em seguida são os elementos adicionados no array
 */
 console.log(aprovados)
